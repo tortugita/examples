0d886afe79929e923b9fcaa68cbddf7240ecd090
Beat Lussi <lussi.beat@gmail.com>
2018-10-15 23:05:21 +0200

    kernel: modifications in example02

--- src/kernel/example02-1.c
+++ src/kernel/example02-1.c
@@ -1,9 +1,15 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/sched.h>
 
 static int hello_init(void)
 {
-  printk(KERN_ALERT "TEST: Hello from kernel module %s\n", __FUNCTION__);
+  struct task_struct *task;
+  for_each_process(task)
+  {  
+    printk(KERN_ALERT "TEST: Hello from kernel module %s\n", __FUNCTION__);
+    printk(KERN_INFO "TEST: process is %s and pid is %d\n", task->comm, task->pid);
+  }
   return 0;
 } 
 
